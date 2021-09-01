ea7cc8269b053e66425478d3060c41ae379e17c0
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-01 11:00:33 +0700

    Added users schemas

---
+++ schemas/types.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from schemas import APIModel
+
+
+class TypesBase(APIModel):
+    id: int
+    name: str
+
+
+class TypesCreateBase(APIModel):
+    name: str
+
+
+class Role(TypesBase):
+    pass
+
+
+class RoleCreate(TypesCreateBase):
+    pass
