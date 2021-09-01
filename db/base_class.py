761d1d3906cccfda104949e19360354d80e76ec9
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-01 09:36:33 +0700

    empty branch main

--- db/base_class.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, INTEGER
-from sqlalchemy.ext.declarative import as_declarative
-
-
-@as_declarative()
-class Base:
-    __name__: str
-
-    id = Column(INTEGER, primary_key=True, autoincrement=True, index=True)
--- run.py
+++
@@ -1,4 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("core.main:app", host="localhost", port=9091, log_level="info")
