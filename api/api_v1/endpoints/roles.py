e463b38bffe5380e71140e8b4ff747ee1e8a9bbc
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-01 10:55:22 +0700

    Added user and role endpoints

---
+++ api/api_v1/endpoints/roles.py
@@ -0,0 +1,77 @@
+from typing import List, Any
+
+from fastapi import APIRouter, Depends
+from starlette.responses import Response
+
+import crud
+import models
+from api import deps
+from sqlalchemy.orm import Session
+
+import schemas
+from api.api_v1.exceptions import NotFoundError
+
+router = APIRouter()
+
+
+@router.post(
+    "/role",
+    response_model=schemas.Role,
+    status_code=201
+)
+async def role_create(
+    role_data: schemas.RoleCreate,
+    db: Session = Depends(deps.get_db),
+    manager: models.User = Depends(deps.get_current_manager_user)
+) -> schemas.Role:
+    return await crud.role.create(db, obj_in=role_data)
+
+
+@router.get(
+    "/roles",
+    response_model=List[schemas.Role]
+)
+async def get_roles(
+        db: Session = Depends(deps.get_db),
+        manager: models.User = Depends(deps.get_current_manager_user),
+) -> Any:
+    return await crud.role.get_multi(db)
+
+
+@router.delete(
+    "/role",
+    status_code=204,
+    responses={
+        403: {"model": schemas.GenericHTTPError, "description": "Permission denied"},
+    },
+)
+async def delete_role(
+        id: int,
+        manager: models.User = Depends(deps.get_current_manager_user),
+        db: Session = Depends(deps.get_db),
+) -> Any:
+    try:
+        await crud.role.remove(db=db, id=id)
+        return Response(status_code=204)
+    except:
+        raise NotFoundError(detail="Role not found")
+
+
+@router.patch(
+    "/role",
+    status_code=200,
+    responses={
+        403: {"model": schemas.GenericHTTPError, "description": "Permission denied"},
+    },
+    response_model=schemas.Role
+)
+async def update_role(
+        id: int,
+        data: schemas.RoleCreate,
+        manager: models.User = Depends(deps.get_current_manager_user),
+        db: Session = Depends(deps.get_db)
+) -> Any:
+    role = await crud.role.get(db=db, id=id)
+    if role is None:
+        raise NotFoundError("Role not found")
+    return await crud.zone_type.update(db=db, db_obj=role, obj_in=data)
---
+++ api/deps.py
@@ -0,0 +1,83 @@
+from typing import Generator, Optional
+
+from broadcaster import Broadcast
+from fastapi import Cookie, Depends, HTTPException, Query, WebSocket, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+import crud
+import models
+import schemas
+from core import security
+from core.config import settings
+from db.session import SessionLocal
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+reusable_oauth2 = OAuth2PasswordBearer(tokenUrl=f"{settings.API_PREFIX}/login/oauth")
+broadcast = Broadcast(settings.REDIS_URI)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+def get_broadcast() -> Generator:
+    yield broadcast
+
+
+async def get_user_by_token(db: Session, token: str):
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = schemas.TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = await crud.user.get(db, id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+async def get_current_user(
+        db: Session = Depends(get_db),
+        token: str = Depends(reusable_oauth2)
+) -> models.User:
+    return await get_user_by_token(db, token)
+
+
+async def get_current_admin_user(
+        current_user: models.User = Depends(get_current_user),
+) -> models.User:
+    if not await crud.user.is_admin_user(current_user):
+        raise HTTPException(
+            status_code=403, detail="The user doesn't have enough privileges"
+        )
+    return current_user
+
+
+async def get_current_manager_user(
+        current_user: models.User = Depends(get_current_user),
+) -> models.User:
+    if not await crud.user.is_manager_user(current_user):
+        raise HTTPException(
+            status_code=403, detail="The user doesn't have enough privileges"
+        )
+    return current_user
+
+
+async def get_cookie_or_token(
+        websocket: WebSocket,
+        session: Optional[str] = Cookie(None),
+        token: Optional[str] = Query(None),
+):
+    if session is None and token is None:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token
