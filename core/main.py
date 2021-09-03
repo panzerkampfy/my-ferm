5f02e704497597235e9fa5c2440413441f67e5ab
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-03 11:14:15 +0700

    user endpoints tests

---
+++ core/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from api.api_v1.api import router
+from api.deps import broadcast
+from core.config import settings
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(title=settings.PROJECT_NAME)
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(router, prefix=settings.API_PREFIX)
---
+++ tests/factories.py
@@ -0,0 +1,66 @@
+import factory
+import models
+from db import session
+
+
+class BaseMeta:
+    sqlalchemy_session = session.SessionLocal
+    sqlalchemy_session_persistence = "flush"
+
+
+class BaseType(factory.alchemy.SQLAlchemyModelFactory):
+    name = "test messages"
+
+
+class RoleFactory(BaseType, factory.alchemy.SQLAlchemyModelFactory):
+    class Meta(BaseMeta):
+        model = models.Role
+
+
+class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta(BaseMeta):
+        model = models.User
+
+    first_name = "пользователь"
+    middle_name = "пользователь"
+    last_name = "пользователь"
+    login = factory.Sequence(lambda n: "test-user-%d" % n)
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        return super()._create(model_class, *args, **kwargs)
+
+    @factory.post_generation
+    def role_name(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            self.role = (
+                UserFactory._meta.sqlalchemy_session.query(models.Role)
+                    .filter(models.Role.name == extracted)
+                    .one()
+            )
+            UserFactory._meta.sqlalchemy_session.flush()
+
+
+class TokenFactory:
+    def __init__(self, client):
+        self.client = client
+
+    def create(self, login: str, password: str):
+        login_data = {
+            "login": login,
+            "password": password,
+        }
+        response = self.client.post(
+            "/api/v1/token", json=login_data
+        )
+        token = response.json()["accessToken"]
+        return token
+
+
+class TokenHeadersFactory(TokenFactory):
+    def create(self, login: str, password: str):
+        token = super().create(login, password)
+        return {"Authorization": f"Bearer {token}"}
