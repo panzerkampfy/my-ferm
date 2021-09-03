27d4e395a502e3df95fff5924667cfe93daa6f24
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-03 10:57:35 +0700

    user and role endpoints

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from api.api_v1.endpoints import login, users, roles
 
 router = APIRouter()
-
+router.include_router(login.router, tags=["login"])
+router.include_router(roles.router, tags=["roles"])
+router.include_router(users.router, tags=["users"])
