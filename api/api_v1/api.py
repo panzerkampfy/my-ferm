f8dc5f917d463fdcb492df91d6b7b7905273049d
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-03 11:59:22 +0700

    storage endpoint and migration

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from api.api_v1.endpoints import login, users, roles, zones
+from api.api_v1.endpoints import login, users, roles, zones, storage
 
 router = APIRouter()
 router.include_router(login.router, tags=["login"])
 router.include_router(roles.router, tags=["roles"])
 router.include_router(users.router, tags=["users"])
 router.include_router(zones.router, tags=["zones"])
+router.include_router(storage.router, tags=["storage"])
