d802c95b50dc732e69d7cfeb66bf24ef9aa0e6cc
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-03 14:10:29 +0700

    product zone endpoints

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.api_v1.endpoints import login, users, roles, zones, storage, products, jobs, storage_job
+from api.api_v1.endpoints import login, users, roles, zones, storage, products, jobs, storage_job, product_zone
 
 router = APIRouter()
 router.include_router(login.router, tags=["login"])
@@ -10,3 +10,4 @@ router.include_router(storage.router, tags=["storage"])
 router.include_router(products.router, tags=["products"])
 router.include_router(jobs.router, tags=["jobs"])
 router.include_router(storage_job.router, tags=["storage job"])
+router.include_router(product_zone.router, tags=["product zone"])
