7b2bf5e749a5775bb9d306db5b29ecdd1c4c535a
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-03 10:45:02 +0700

    user and role crud, migration

--- core/config.py
+++ core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     SERVER_PORT: str = "8012"
     HOST_URI: str = f"http://{SERVER_HOST}:{SERVER_PORT}"
     API_PREFIX: str = "/api/v1"
-    SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:1234@localhost:5432/myferm"
+    SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:1234@localhost:5432/myferm3"
     REDIS_URI: str = "redis://redis:6379"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
     BACKEND_CORS_ORIGINS: List[str] = ["*"]
