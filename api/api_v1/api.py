1e2c0e394a693fd02eb1d5231eb6b25a0649fbf1
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-03 13:48:48 +0700

    storage job endpoints

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.api_v1.endpoints import login, users, roles, zones, storage, products, jobs
+from api.api_v1.endpoints import login, users, roles, zones, storage, products, jobs, storage_job
 
 router = APIRouter()
 router.include_router(login.router, tags=["login"])
@@ -9,3 +9,4 @@ router.include_router(zones.router, tags=["zones"])
 router.include_router(storage.router, tags=["storage"])
 router.include_router(products.router, tags=["products"])
 router.include_router(jobs.router, tags=["jobs"])
+router.include_router(storage_job.router, tags=["storage job"])
