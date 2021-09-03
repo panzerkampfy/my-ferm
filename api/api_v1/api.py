4d51ece598cf14cf04ca573a1672339d7f43825e
Pyatnichuk Ivan <panzerkampfy@gmail.com>
2021-09-03 12:15:28 +0700

    added product crud and endpoints

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.api_v1.endpoints import login, users, roles, zones, storage
+from api.api_v1.endpoints import login, users, roles, zones, storage, products
 
 router = APIRouter()
 router.include_router(login.router, tags=["login"])
@@ -7,3 +7,4 @@ router.include_router(roles.router, tags=["roles"])
 router.include_router(users.router, tags=["users"])
 router.include_router(zones.router, tags=["zones"])
 router.include_router(storage.router, tags=["storage"])
+router.include_router(products.router, tags=["products"])
